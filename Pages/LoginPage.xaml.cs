8c171cb6b349e52eb41dd312de114e0d21c06dff
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-27 10:13:24 +0100

    Wyglad register & login ✅

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -1,9 +1,30 @@
-namespace loginAppAS;
-
-public partial class LoginPage : ContentPage
+namespace loginAppAS
 {
-	public LoginPage()
-	{
-		InitializeComponent();
-	}
+    public partial class LoginPage : ContentPage
+    {
+
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string email = emailEntry.Text;
+            string password = passwordEntry.Text;
+
+            // ... (jak w poprzednim kodzie, ale skoncentrowane na logowaniu)
+
+            Login(email, password);
+        }
+
+        private void Login(string email, string password)
+        {
+            // Implementacja logowania
+            // ...
+
+            DisplayAlert("Login udany", "", "Ok");
+            Navigation.PushAsync(new WelcomePage());
+        }
+    }
 }
--- Pages/RegisterPage.xaml.cs
+++ Pages/RegisterPage.xaml.cs
@@ -1,9 +1,30 @@
-namespace loginAppAS;
-
-public partial class RegisterPage : ContentPage
+namespace loginAppAS
 {
-	public RegisterPage()
-	{
-		InitializeComponent();
-	}
+    public partial class RegisterPage : ContentPage
+    {
+
+        public RegisterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+            string email = emailEntry.Text;
+            string password = passwordEntry.Text;
+            string confirmPassword = confirmPasswordEntry.Text;
+
+            // ... (jak w poprzednim kodzie, ale skoncentrowane na rejestracji)
+
+            Register(email, password);
+        }
+
+        private void Register(string email, string password)
+        {
+            // Implementacja rejestracji
+            // ...
+
+            DisplayAlert("Rejestracja udana", "", "Ok");
+        }
+    }
 }
