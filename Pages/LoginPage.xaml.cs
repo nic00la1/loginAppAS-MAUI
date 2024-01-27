a8c205d150f80abeb353897daee6dd4472056bcf
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-27 11:07:28 +0100

    Dodano ikonki ✅

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -7,38 +7,6 @@ namespace loginAppAS
             InitializeComponent();
         }
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
-        {
-            string email = emailEntry.Text;
-            string password = passwordEntry.Text;
 
-            if (string.IsNullOrEmpty(email) || !email.Contains("@"))
-            {
-                errorLabel.Text = "Podaj poprawny adres e-mail";
-                return;
-            }
-
-            if (string.IsNullOrEmpty(password))
-            {
-                errorLabel.Text = "Wpisz has³o";
-                return;
-            }
-
-            // Implementacja logowania
-            Login(email, password);
-        }
-
-        private void Login(string email, string password)
-        {
-            // Tutaj powinna byæ logika wys³ania ¿¹dania do serwera
-            // w celu autoryzacji u¿ytkownika. To jest miejsce,
-            // gdzie normalnie odbywa³aby siê komunikacja z backendem.
-
-            // Jeœli logowanie zakoñczy siê sukcesem, mo¿esz
-            // wyœwietliæ odpowiednie powiadomienie i przenieœæ
-            // u¿ytkownika do strony powitalnej.
-            DisplayAlert("Logowanie udane", "", "Ok");
-            Navigation.PushAsync(new WelcomePage());
-        }
     }
 }
--- Pages/MainPage.xaml.cs
+++ Pages/MainPage.xaml.cs
@@ -8,14 +8,7 @@
             InitializeComponent();
         }
 
-        private void RegisterButton_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new RegisterPage());
-        }
 
-        private void LoginButton_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new LoginPage());
-        }
+
     }
 }
