9abba48d2292886e20500490e3896868fd67fe72
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-27 12:19:14 +0100

    Dodano tymczasową obsługę logiki logowania ✅

--- Pages/LoginPage.xaml.cs
+++ Pages/LoginPage.xaml.cs
@@ -12,5 +12,49 @@ namespace loginAppAS
             await Shell.Current.GoToAsync("//Register");
         }
 
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string email = emailEntry.Text;
+            string password = passwordEntry.Text;
+
+            if (string.IsNullOrEmpty(email) || !email.Contains("@"))
+            {
+                errorLabel.Text = "Podaj poprawny adres e-mail";
+                return;
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                errorLabel.Text = "Wpisz has³o";
+                return;
+            }
+
+            // Implementacja logowania
+            bool loginSuccess = await Login(email, password);
+
+            if (loginSuccess)
+            {
+                // Po udanym zalogowaniu, mo¿esz dodatkowo przekierowaæ u¿ytkownika
+                // do strony powitalnej, itp.
+                _ = Navigation.PushAsync(new WelcomePage());
+            }
+            else
+            {
+                errorLabel.Text = "B³¹d logowania. Spróbuj ponownie.";
+            }
+        }
+
+        private async Task<bool> Login(string email, string password)
+        {
+            // Tutaj powinna byæ logika wys³ania ¿¹dania do serwera
+            // w celu autoryzacji u¿ytkownika. To jest miejsce,
+            // gdzie normalnie odbywa³aby siê komunikacja z backendem.
+
+            // Oto przyk³ad symulacji, u¿ywaj¹c Task.Delay
+            await Task.Delay(2000);
+
+            // Symulacja sukcesu logowania
+            return true;
+        }
     }
 }
