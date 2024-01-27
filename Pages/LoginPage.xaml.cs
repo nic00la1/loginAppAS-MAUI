4429f064b65222990ee353c865ed795aed727d11
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-27 10:08:16 +0100

    Dziala przenoszenie z MainPage do podanych Podstron ✅

---
+++ Pages/LoginPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace loginAppAS;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pages/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace loginAppAS
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new RegisterPage());
+        }
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
