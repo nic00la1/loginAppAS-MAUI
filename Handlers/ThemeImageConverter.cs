96ed7f62aab17b6ac5445439bd64ff70ec13e039
nic00la1 <nic00la.kaleta@gmail.com>
2024-01-28 17:03:36 +0100

    WelcomePage ikonka usera zmienia sie w zaleznosci od theme uzytkownika ✅💪

--- Handlers/ThemeImageConverter.cs
+++ Handlers/ThemeImageConverter.cs
@@ -12,17 +12,39 @@ namespace loginAppAS.Handlers
             // Zwróć odpowiednie źródło obrazu w zależności od motywu
             if (appTheme == AppTheme.Light)
             {
-                return ImageSource.FromFile("user.png");
+                return ImageSource.FromFile(GetLightThemeImage(value));
             }
             else
             {
-                return ImageSource.FromFile("password.png");
+                return ImageSource.FromFile(GetDarkThemeImage(value));
             }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
+        }
+
+        private string GetLightThemeImage(object value)
+        {
+            // Logika do pobrania odpowiedniego obrazu dla motywu jasnego
+            // Możesz użyć wartości value do określenia, który obraz jest potrzebny
+            // Na przykład, jeśli value to "user", możesz zwrócić "user_light.png"
+            // Jeśli value to "password", możesz zwrócić "password_light.png"
+            // Dodaj odpowiednie warunki dla innych obrazów
+
+            return "user_light.png"; // Zwróć ścieżkę do obrazu dla motywu jasnego
+        }
+
+        private string GetDarkThemeImage(object value)
+        {
+            // Logika do pobrania odpowiedniego obrazu dla motywu ciemnego
+            // Możesz użyć wartości value do określenia, który obraz jest potrzebny
+            // Na przykład, jeśli value to "user", możesz zwrócić "user_dark.png"
+            // Jeśli value to "password", możesz zwrócić "password_dark.png"
+            // Dodaj odpowiednie warunki dla innych obrazów
+
+            return "user_dark.png"; // Zwróć ścieżkę do obrazu dla motywu ciemnego
         }
     }
 }
